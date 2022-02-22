5db609583e31d1577b55acf848eaeb8a4234433b
Alice Hsing <alicehsing@gmail.com>
2022-02-21 16:23:12 -0800

    added car_make and removed pet to/from data set

--- src/data-utils.test.js
+++ src/data-utils.test.js
@@ -4,12 +4,12 @@ test('generateColumns function should make a nice array of columns that capitali
   const expected = [
     { key: 'first_name', name: 'First Name' },
     { key: 'age', name: 'Age' },
-    { key: 'pet', name: 'Pet' },
+    { key: 'car_make', name: 'Car Make' },
   ];
 
   const actual = generateColumns([
-    { first_name: 'Sally', age: 22, pet: 'American Bulldog' },
-    { first_name: 'Leslie', age: 3, pet: 'Caribou' }
+    { first_name: 'Sally', age: 22, car_make: 'Audi' },
+    { first_name: 'Leslie', age: 3, pet: 'Toyota' }
   ]);
 
   expect(actual).toEqual(expected);
