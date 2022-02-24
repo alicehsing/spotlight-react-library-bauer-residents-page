c861c3269bfdb4f3bbe2b384e7aa3f353c1203b8
Alice Hsing <alicehsing@gmail.com>
2022-02-23 18:08:39 -0800

    styled bar chart

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import DataGrid from 'react-data-grid';
 import data from './data';
 import './App.css';
 import { generateColumns, getTotalUserOfEachCarMake } from './data-utils';
-import { VictoryBar, VictoryChart, } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
 import { VictoryTheme } from 'victory';
 
 
@@ -13,13 +13,32 @@ export default function App() {
         columns={generateColumns(data)}
         rows={data} />
     </div>
-    <div>
+    <div style={{ height: 1000 }} className="bar-chart">
+      <h2>Total Number of Users Per Car Brand</h2>
       <VictoryChart
         theme={VictoryTheme.material}
-        domainPadding={10}>
-        <VictoryBar
-          style={{ data: { fill: '#c43a31' } }}
+        domainPadding={{ x: 5 }}
+        width={500}
+        height={600}
+      >
+        <VictoryAxis
+          dependentAxis={true}
+          style={{
+            grid: { stroke: 'grey' }
+          }}
+        />
+        <VictoryAxis />
+        <VictoryBar horizontal
+          style={{ 
+            data: { fill: '#c43a31' },
+            labels: { fontSize: ({ text }) => text.length > 10 ? 8 : 10 }
+          }}
           data={getTotalUserOfEachCarMake(data)}
+          labels={({ datum }) => `${datum.y}`}
+          animate={{
+            duration: 2000,
+            onLoad: { duration: 1000 }
+          }}
         />
       </VictoryChart>  
     
